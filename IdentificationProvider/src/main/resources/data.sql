62f113ddd737b0e8f0fe66291e4b210e18cc58b5
Rajo Gajic <racog3@gmail.com>
2017-02-16 14:46:33 +0100

    Database initialisation with test data

---
+++ IdentificationProvider/src/main/resources/data.sql
@@ -0,0 +1,30 @@
+-- -----------------------------------------------------
+-- Spring Security related tables and roles (roles which it has in SSO server Security Context)
+-- -----------------------------------------------------
+
+SET @user1 = 'user1@etfbl.net', @user2 = 'user2@etfbl.net', @pass1 = 1234, @pass2=1234;
+SET @host1Url = 'http://localhost:8081/sp1/login', @host2Url = 'http://localhost:8082/sp2/login';
+
+INSERT INTO `ssodb`.`users` (`username`, `password`, `enabled`) VALUES (@user1, @pass1, '1');
+INSERT INTO `ssodb`.`users` (`username`, `password`, `enabled`) VALUES (@user2, @pass2, '1');
+
+INSERT INTO `ssodb`.`user_roles` (`username`, `role`) VALUES (@user1, 'ROLE_USER');
+INSERT INTO `ssodb`.`user_roles` (`username`, `role`) VALUES (@user2, 'ROLE_USER');
+
+-- -----------------------------------------------------
+-- SSO service tables and roles (roles which it has n requested Service Provider - SP application)
+-- -----------------------------------------------------
+INSERT INTO `ssodb`.`target_hosts` (`url`, `name`) VALUES (@host1Url, 'SP1');
+INSERT INTO `ssodb`.`target_hosts` (`url`, `name`) VALUES (@host2Url, 'SP2');
+
+-- user1 has access to protected resource on both SSO client apss (SP's)
+INSERT INTO `ssodb`.`target_authorities` (`username`, `target_host_id`, `role`) VALUES
+    (@user1, (SELECT `target_host_id` FROM `ssodb`.`target_hosts` WHERE `url` = @host1Url) , 'ROLE_USER');
+INSERT INTO `ssodb`.`target_authorities` (`username`, `target_host_id`, `role`) VALUES
+    (@user1, (SELECT `target_host_id` FROM `ssodb`.`target_hosts` WHERE `url` = @host2Url) , 'ROLE_USER');
+
+-- user2 has access to protected resource only on 2nd SSO client app (SP2)
+INSERT INTO `ssodb`.`target_authorities` (`username`, `target_host_id`, `role`) VALUES
+    (@user2, (SELECT `target_host_id` FROM `ssodb`.`target_hosts` WHERE `url` = @host1Url) , 'ROLE_GUEST');
+INSERT INTO `ssodb`.`target_authorities` (`username`, `target_host_id`, `role`) VALUES
+    (@user2, (SELECT `target_host_id` FROM `ssodb`.`target_hosts` WHERE `url` = @host2Url) , 'ROLE_USER');
--- IdentificationProvider/src/main/resources/schema.sql
+++ IdentificationProvider/src/main/resources/schema.sql
@@ -1,21 +1,9 @@
 -- -----------------------------------------------------
 -- Schema ssodb
 -- -----------------------------------------------------
+DROP SCHEMA IF EXISTS `ssodb`;
 CREATE SCHEMA IF NOT EXISTS `ssodb` DEFAULT CHARACTER SET utf8 ;
 USE `ssodb` ;
-
--- -----------------------------------------------------
--- Table `ssodb`.`target_hosts`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `ssodb`.`target_hosts` (
-  `target_host_id` INT(11) NOT NULL AUTO_INCREMENT,
-  `url` VARCHAR(255) NOT NULL,
-  `name` VARCHAR(45) NOT NULL,
-  PRIMARY KEY (`target_host_id`))
-ENGINE = InnoDB
-AUTO_INCREMENT = 2
-DEFAULT CHARACTER SET = utf8;
-
 
 -- -----------------------------------------------------
 -- Table `ssodb`.`users`
@@ -25,31 +13,6 @@ CREATE TABLE IF NOT EXISTS `ssodb`.`users` (
   `password` VARCHAR(45) NOT NULL,
   `enabled` TINYINT(4) NOT NULL DEFAULT '1',
   PRIMARY KEY (`username`))
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8;
-
-
--- -----------------------------------------------------
--- Table `ssodb`.`target_authorities`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `ssodb`.`target_authorities` (
-  `target_authority_id` INT(11) NOT NULL,
-  `username` VARCHAR(45) NOT NULL,
-  `target_host_id` INT(11) NOT NULL,
-  `role` VARCHAR(45) NOT NULL,
-  PRIMARY KEY (`target_authority_id`, `username`, `target_host_id`),
-  INDEX `fk_target_authorities_users1_idx` (`username` ASC),
-  INDEX `fk_target_authorities_target_hosts1_idx` (`target_host_id` ASC),
-  CONSTRAINT `fk_target_authorities_target_hosts1`
-    FOREIGN KEY (`target_host_id`)
-    REFERENCES `ssodb`.`target_hosts` (`target_host_id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_target_authorities_users1`
-    FOREIGN KEY (`username`)
-    REFERENCES `ssodb`.`users` (`username`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8;
 
@@ -69,4 +32,42 @@ CREATE TABLE IF NOT EXISTS `ssodb`.`user_roles` (
     REFERENCES `ssodb`.`users` (`username`))
 ENGINE = InnoDB
 AUTO_INCREMENT = 2
+DEFAULT CHARACTER SET = utf8;
+
+
+-- -----------------------------------------------------
+-- Table `ssodb`.`target_hosts`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `ssodb`.`target_hosts` (
+  `target_host_id` INT(11) NOT NULL AUTO_INCREMENT,
+  `url` VARCHAR(255) NOT NULL,
+  `name` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`target_host_id`))
+ENGINE = InnoDB
+AUTO_INCREMENT = 2
+DEFAULT CHARACTER SET = utf8;
+
+
+-- -----------------------------------------------------
+-- Table `ssodb`.`target_authorities`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `ssodb`.`target_authorities` (
+  `target_authority_id` INT(11) NOT NULL AUTO_INCREMENT,
+  `username` VARCHAR(45) NOT NULL,
+  `target_host_id` INT(11) NOT NULL,
+  `role` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`target_authority_id`, `username`, `target_host_id`),
+  INDEX `fk_target_authorities_users1_idx` (`username` ASC),
+  INDEX `fk_target_authorities_target_hosts1_idx` (`target_host_id` ASC),
+  CONSTRAINT `fk_target_authorities_target_hosts1`
+    FOREIGN KEY (`target_host_id`)
+    REFERENCES `ssodb`.`target_hosts` (`target_host_id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_target_authorities_users1`
+    FOREIGN KEY (`username`)
+    REFERENCES `ssodb`.`users` (`username`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8;
